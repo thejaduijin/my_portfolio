6ea3fa9ad5cbc42aee15c6e4f09b6ee56d5a95f6
Lokendra Singh <thejaduijin@gmail.com>
2023-08-31 16:20:56 +0530

    portfolio with next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h1>Hello, I'm Lokendra Singh</h1>
+      </div>
+    </main>
+  )
+}
